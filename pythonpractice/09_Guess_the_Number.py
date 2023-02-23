6400101659b9cbacf0be3828f60498c2b4ebcd05
NIGEL ONG <nigel.ong00@gmail.com>
2023-02-23 06:32:29 -0800

    scuffed 09

--- pythonpractice/09_Guess_the_Number.py
+++ pythonpractice/09_Guess_the_Number.py
@@ -1,21 +1,23 @@
 import random
-x= random.randint(1,10)
-count =[]
-print(x)
+rn= random.randint(1,10)
+counter = 0
+print(rn)
 
-a = 0
-running = True
+guess = 0
 
-while running:
 
-    a = int(input("guess1"))
+while guess != rn :
+    guess = input("Guess the number between 1-10: ") 
+    while guess not in ["1","2","3","4","5","6","7","8","9","10"]:
+        print("invalid")
 
-    if a == 55:
-        running = False
-    if a < x:
-        print("too low")
-    elif a > x:
-        print("too high")
+    if int(guess) < rn:
+        print("Too low")
+        counter += 1
+    elif int(guess) > rn:
+        print("Too high")
+        counter += 1
     else:
-        print("you found da number")
-input()
+        counter += 1
+        print(f"You found da number, you took {counter} guesses")
+        
