525bddb0bf87344524c6ce7d34e021ccf634228d
Nigel <nigel.ong00@gmail.com>
2023-03-06 18:34:27 -0800

    recent update for store and database

--- Week9/store/app.py
+++ Week9/store/app.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from flask import Flask, jsonify, render_template, request
 
 from database import db
-from models import Product
+from models import Product, Order, ProductsOrder
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///store.db"
@@ -68,32 +68,106 @@ def api_put_product(name):
         product.price = data["price"]
     else:
         product.price = product.price
-    # if "quantity" in data.keys():
-    #     product.quantity = data["quantity"]
+    if "quantity" in data.keys():
+        product.quantity = data["quantity"]
+    else:
+        product.quantity = product.quantity 
 
-    # if data.quantity in data:
-    #     product.quantity = data["quantity"]
-    # else:
-    #     product.quantity = product.quantity 
-
-    # try:
-    #     if not isinstance(data["quantity"],int) or int(data["quantity"]) < 0:
-    #         raise AttributeError
-    #     elif not isinstance(data["price"],float) or float(data["price"]) < 0:
-    #         raise ValueError
-    # except ValueError:
-    #     return (
-    #         "Invalid float values: price must be a positive or float",
-    #         400,
-    #     )
-    # except AttributeError:
-    #     return (
-    #         "Invalid int values: Not an positive or integer",
-    #         400,
-    #     )
+    try:
+        if "quantity" in data.keys():
+            if not isinstance(data["quantity"],int) or int(data["quantity"]) < 0:
+                raise AttributeError
+        if "price" in data.keys():
+            if not isinstance(data["price"],float) or float(data["price"]) < 0:
+                raise ValueError
+    except ValueError:
+        return (
+            "Invalid float values: price must be a positive or float",
+            400,
+        )
+    except AttributeError:
+        return (
+            "Invalid int values: Not an positive or integer",
+            400,
+        )
     db.session.add(product)
     db.session.commit()
     return "Updated"
+
+@app.route("/api/order/<int:order_id>", methods=["GET"])
+def api_get_order(order_id):
+    order = db.session.get(Order, order_id)
+    if not order:
+        return ("Order Not found"), 404
+    order_json = order.to_dict()
+    return jsonify(order_json)
+
+@app.route("/api/order", methods=["POST"])
+def api_create_order():
+    data = request.json
+    # Check all data is provided
+    for key in ("customer_name", "customer_address", "products"):
+        if key not in data:
+            return f"The JSON provided is invalid (missing: {key})", 400
+
+    try:
+        if not data["products"]:
+            raise AttributeError
+    
+    except AttributeError:
+        return (
+            "The app should accept orders with products that exist in the inventory, even if the quantity available is 0",
+            400,
+        )
+
+    order = Order(
+        name = data["customer_name"],
+        address=data["customer_address"]
+    )
+
+        
+    for prod in data["products"]:
+        product = db.session.get(Product, prod["name"])
+        quantity = prod["quantity"]
+        if not product:
+            return f"{prod['name']} is not in stock", 400
+        if not isinstance(quantity,int) or quantity < 0 :
+            return f"Number must be whole and positive", 400
+        
+        po = ProductsOrder(product=product, order=order,quantity= quantity)
+        db.session.add(po)
+        
+
+    db.session.add(order)
+    db.session.commit()
+    return jsonify(order.to_dict())
+    #return api_get_order()
+
+@app.route("/api/order/<int:order_id>", methods=["PUT"])
+def api_process_order(order_id):
+    order = db.session.get(Order, order_id)
+    data=request.json
+    for key in ("process",):
+        if key not in data:
+            return f"The JSON payload is invalid (missing: {key})", 400
+    if not order:
+        return ("Order Not found"), 404
+    if data["process"]:
+        if order.completed is False:
+            for ordered_products in order.products:
+                product = ordered_products.product 
+                if product.quantity < ordered_products.quantity:
+                    ordered_products.quantity = product.quantity
+                product.quantity -= ordered_products.quantity 
+            order.completed = True
+        else:
+            return f"Order has been previously processed", 400
+    db.session.commit()
+    return jsonify(order.to_dict()) 
+                
+
+
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Week9/store/models.py
+++ Week9/store/models.py
@@ -9,3 +9,33 @@ class Product(db.Model):
     def to_dict(self):
         product_dict = {"name": self.name,"price": self.price,"quantity": self.quantity}
         return product_dict
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    address = db.Column(db.String, nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
+    products = db.relationship('ProductsOrder', back_populates='order')
+
+    def to_dict(self):
+        pro_dict = []
+        price = 0
+        for product in self.products:
+            pro_dict.append({"name": product.product.name,"quantity": product.quantity})
+            price += product.quantity * product.product.price
+
+        order_dict = {
+            "customer_name":self.name,
+            "customer_address":self.address,
+            "products": pro_dict,
+            "price": round(price,2)
+        }
+        return order_dict
+
+class ProductsOrder(db.Model):
+    product_name = db.Column(db.ForeignKey("product.name"), primary_key =True)
+    order_id = db.Column(db.ForeignKey("order.id"), primary_key =True)
+    quantity = db.Column(db.Integer, nullable=False)
+
+    product = db.relationship('Product')
+    order = db.relationship('Order', back_populates='products')
